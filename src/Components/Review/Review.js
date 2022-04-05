45488a07abb70edfbef59f01634877d207891c21
haniful-islam <haniful360@gmail.com>
2022-04-05 21:46:43 +0600

    ubdate

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -5,7 +5,7 @@ const Review = ({ review }) => {
     const { name, comment, ratting } = review;
     return (
         <div className=''>
-            <div className='p-5 m-5 border-2 border-red-600 rounded bg-red-100'>
+            <div className='p-5 m-5 border-2 border-purple-600 rounded bg-red-100'>
                 <h2 className='text-xl font-bold'>{name}</h2>
                 <p className='my-3'>{comment}</p>
                 <p>Ratting: <span className='text-red-600 font-bold'>{ratting}</span></p>
